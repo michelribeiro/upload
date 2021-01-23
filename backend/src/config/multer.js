38e331b79b34595a40e41d6d46eda62c737bf94d
Michel Lima Ribeiro <michelribeiro2004@gmail.com>
2021-01-23 00:47:16 -0300

    aplicacao de upload de imagens com node para aws

--- backend/src/config/multer.js
+++ backend/src/config/multer.js
@@ -1,23 +1,40 @@
 const multer = require('multer');
 const path = require('path');
 const crypto = require('crypto');
+const multerS3 = require("multer-s3");
+const aws = require("aws-sdk");
 
-module.exports = {
-    dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
-    storage: multer.diskStorage({
+const storageTypes = {
+    local: multer.diskStorage({
         destination: (req, file, cb) => {
             cb(null, path.resolve(__dirname, '..', '..', 'tmp', 'uploads'))
         },
         filename: (req, file, cb) => {
             crypto.randomBytes(16, (err, hash) => {
                 if(err) cb(err)
-
-                const fileName = `${hash.toString('hex')}-${file.originalname}`;
-
-                cb(null, fileName);
-            })
+                file.key = `${hash.toString('hex')}-${file.originalname}`;
+                cb(null, file.key);
+            });
         }
     }),
+    s3: multerS3({
+        s3: new aws.S3(),
+        bucket: 'uploadimgapp',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        acl: 'public-read',
+        key: (req, file, cb) => {
+            crypto.randomBytes(16, (err, hash) => {
+                if(err) cb(err)
+                const fileName = `${hash.toString('hex')}-${file.originalname}`;
+                cb(null, fileName);
+            });
+        }
+    })
+}
+
+module.exports = {
+    dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+    storage: storageTypes[process.env.STORAGE_TYPE],
     limits: {
         fileSize: 2 * 1024 * 1024,
     },
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const path = require("path");
 
 const app = express();
 
-mongoose.connect("mongodb+srv://michel:d16m08a1981@cluster0-aeenb.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true", {
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
@@ -12,6 +14,10 @@ mongoose.connect("mongodb+srv://michel:d16m08a1981@cluster0-aeenb.mongodb.net/te
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
+app.use(
+    "/files",
+    express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
+);
 
 app.use(require("./routes"));
 
